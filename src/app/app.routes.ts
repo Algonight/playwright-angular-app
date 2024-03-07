8ec50e2217936e00994c371b5fa80bc1e037a84b
Algonight <benjamin.fournet99@gmail.com>
2024-03-07 14:25:59 +0100

    refactor: rearange components and add login and not-found components
      - Move router-outlet to app component
      - Move main content in home component
      - Fix styles
      - Add login and not-found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'not-found' },
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
